174ddb61aa547f2f408a862f8746a177d6baae3f
get-manna <mujiburrahmanmanna@gmail.com>
2024-05-27 06:33:23 -0700

    first commit

---
+++ src/Component/Boox.jsx
@@ -0,0 +1,24 @@
+
+const Boox = () => {
+  return (
+    <div className=" ">
+    
+    <div className="flex">
+
+    
+    <div className="border-2	border-green-300">
+          <div className="pt-5 pb-5 px-5 ">
+            <h2 className='text-black text-2xl	 pb-3'>Learn react </h2>
+            <p className=' text-black text-1xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum, nihil.</p>
+          </div>
+        </div>
+
+       
+    
+    </div>
+
+    </div>
+  )
+}
+
+export default Boox
---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,26 @@
+import Header from "./Header"
+import Boox from "../Component/Boox"
+import Footer from "./Footer"
+
+const Layout = () => {
+  return (
+    <>
+
+    <Header/>
+
+    <Boox/>
+
+    <Boox/>
+
+    <Boox/>
+
+
+
+
+    <Footer/>
+      
+    </>
+  )
+}
+
+export default Layout
