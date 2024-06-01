dba7877674c1fc25603d67ef87dc19f1c2dec087
get-manna <mujiburrahmanmanna@gmail.com>
2024-06-01 06:26:11 -0700

    useeffect-2

--- src/App.jsx
+++ src/App.jsx
@@ -45,13 +45,13 @@ const App = () => {
 
   return (
     <div className="max-w-[1440px] my-8">
-      <h1 className="text-2xl text-center mb-5">Product List</h1>
+      <h1 className=" text-center text-blue-500 uppercase text-3xl mb-5">Product List</h1>
     
       {
         Producttwo.length  ? (
           <Producttwo products={Producttwo} /> 
         ) : (
-          <p>Loading  product ...</p>
+          <p className="text-bold uppercase text-2xl">Loading  product ...</p>
         )
       }
     </div>
